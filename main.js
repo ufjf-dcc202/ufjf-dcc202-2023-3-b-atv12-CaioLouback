f8162e720beaeab841d353491b5dc00158ad6560
Caio Louback <caio.louback@estudante.ufjf.br>
2023-12-05 21:46:56 -0300

    algumas  alteracoes no main e estoque

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
+import { getEstoque } from "./estoque.js";
+
 const olJoao = document.querySelector("#joao");
 const olMaria = document.querySelector("#maria");
 
 document.entrada.addEventListener('submit', leFormulario);
+
+atualizaTela();
 
 function leFormulario(event){
     event.preventDefault();
@@ -13,3 +17,14 @@ function leFormulario(event){
     console.log(`${origem} doa ${quantidade} ${fruta} para ${destino}`);
 }
 
+
+function atualizaTela(){
+    const estoque = getEstoque();
+    olJoao.innerHTML = "";
+    for(let i =0; i < estoque.joao.length; i++){
+        const monte = estoque.joao[i];
+        const li = document.createElement('li');
+        li.textContent = `${monte.tipo}: ${monte.qtd}`;
+        olJoao.append(li);
+    }
+}
